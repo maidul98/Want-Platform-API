f8c9fc61dc8cb63acf14189af401eead3ee5510a
Maidul Islam <maidul98@gmail.com>
2019-01-18 15:00:47 -0500

    Google login in updated

--- app/Http/Controllers/PassportController.php
+++ app/Http/Controllers/PassportController.php
@@ -122,6 +122,7 @@ class PassportController extends Controller
         
     }
 
+
     /**
      * Change password 
      * input: current-password, new-password
@@ -163,32 +164,40 @@ class PassportController extends Controller
     public function handleProviderCallback()
     {
         try {
-            $user = Socialite::driver('google')->stateless()->user();
-            return $user->user;
+            $user = Socialite::driver('google')->stateless()->user()->user;
+            // return $user->email;
         } catch (\Exception $e) {
             return $e->getMessage();
         }
-        // // only allow people with @company.com to login
-        // if(explode("@", $user->email)[1] !== 'company.com'){
-        //     return redirect()->to('/');
-        // }
+
         // // check if they're an existing user
-        // $existingUser = User::where('email', $user->email)->first();
-        // if($existingUser){
-        //     // log them in
-        //     auth()->login($existingUser, true);
-        // } else {
-        //     // create a new user
-        //     $newUser                  = new User;
-        //     $newUser->name            = $user->name;
-        //     $newUser->email           = $user->email;
-        //     $newUser->google_id       = $user->id;
-        //     $newUser->avatar          = $user->avatar;
-        //     $newUser->avatar_original = $user->avatar_original;
-        //     $newUser->save();
-        //     auth()->login($newUser, true);
-        // }
-        // return redirect()->to('/home');
+        $existingUser = User::where('email', $user->email)->first();
+        if($existingUser){
+
+            //return token to login the user
+            $token = auth()->user()->createToken('login')->accessToken;
+            return response()->json(['token' => $token], 200);
+        } else {
+            // create a new user
+            User::create([
+                'first_name' => $user->given_name,
+                'last_name' => $user->family_name,
+                'email' => $user->email,
+                'password' => null,
+            ]);
+
+            // $newUser                  = new User;
+            // $newUser->name            = $user->name;
+            // $newUser->email           = $user->email;
+            // $newUser->google_id       = $user->id;
+            // $newUser->avatar          = $user->avatar;
+            // $newUser->avatar_original = $user->avatar_original;
+            // $newUser->save();
+            
+            //return token to login the user
+            $token = auth()->user()->createToken('login')->accessToken;
+            return response()->json(['token' => $token], 200);
+        }
     }
 
 }
