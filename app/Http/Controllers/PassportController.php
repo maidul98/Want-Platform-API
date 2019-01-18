907ee42e7d351acde3ebe1933f86216a231ae286
Maidul Islam <maidul98@gmail.com>
2019-01-18 00:14:43 -0500

    Google Auth

--- app/Http/Controllers/PassportController.php
+++ app/Http/Controllers/PassportController.php
@@ -164,30 +164,31 @@ class PassportController extends Controller
     {
         try {
             $user = Socialite::driver('google')->user();
+            return $user;
         } catch (\Exception $e) {
-            return redirect('/login');
+            return 'user data not found';
         }
-        // only allow people with @company.com to login
-        if(explode("@", $user->email)[1] !== 'company.com'){
-            return redirect()->to('/');
-        }
-        // check if they're an existing user
-        $existingUser = User::where('email', $user->email)->first();
-        if($existingUser){
-            // log them in
-            auth()->login($existingUser, true);
-        } else {
-            // create a new user
-            $newUser                  = new User;
-            $newUser->name            = $user->name;
-            $newUser->email           = $user->email;
-            $newUser->google_id       = $user->id;
-            $newUser->avatar          = $user->avatar;
-            $newUser->avatar_original = $user->avatar_original;
-            $newUser->save();
-            auth()->login($newUser, true);
-        }
-        return redirect()->to('/home');
+        // // only allow people with @company.com to login
+        // if(explode("@", $user->email)[1] !== 'company.com'){
+        //     return redirect()->to('/');
+        // }
+        // // check if they're an existing user
+        // $existingUser = User::where('email', $user->email)->first();
+        // if($existingUser){
+        //     // log them in
+        //     auth()->login($existingUser, true);
+        // } else {
+        //     // create a new user
+        //     $newUser                  = new User;
+        //     $newUser->name            = $user->name;
+        //     $newUser->email           = $user->email;
+        //     $newUser->google_id       = $user->id;
+        //     $newUser->avatar          = $user->avatar;
+        //     $newUser->avatar_original = $user->avatar_original;
+        //     $newUser->save();
+        //     auth()->login($newUser, true);
+        // }
+        // return redirect()->to('/home');
     }
 
 }
