61f548966ae71ac9888128622b045a468c0269a8
Maidul Islam <maidul98@gmail.com>
2019-06-07 05:23:40 -0400

    added rec details

--- config/app.php
+++ config/app.php
@@ -215,7 +215,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        // 'Laracombee' => Amranidev\Laracombee\facades\LaracombeeFacade::class,
+        'Laracombee' => Amranidev\Laracombee\facades\LaracombeeFacade::class,
     ],
 
 ];
