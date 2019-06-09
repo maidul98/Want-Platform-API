4bc4dfdcc4b0e10b58a5d0ae0c01a95a9e35c136
Maidul Islam <maidul98@gmail.com>
2019-06-09 17:59:09 -0400

    updated register to add user to ML DB

--- routes/api.php
+++ routes/api.php
@@ -188,11 +188,19 @@ Route::middleware('auth:api')->group(function () {
 });
 
 
-// Route::get('/ml', function (Request $request) {
-//     $user = User::find(1);
-//     // Prepare the request for recombee server, we need 10 recommended items for a given user.
-//     $recommendations = Laracombee::recommendTo($user, 10)->wait();
-//     return $recommendations->recomms;
-// });
+Route::get('/ml', function (Request $request) {
+    $user = User::find(1);
+    // Prepare the request for recombee server, we need 10 recommended items for a given user.
+    $recommendations = Laracombee::recommendTo($user, 10)->wait();
+    $recc_id = $recommendations['recommId'];
+    $reccs = $recommendations['recomms'];
+    $recommended_ids = [];
+    foreach($reccs as $x){
+        array_push($recommended_ids, $x['id']);
+    }
+    //get array of ids
+
+    return Want::findMany($recommended_ids);
+});
 
 
