017a5da1fa728a6f2e2ef29b6c14baf9d2e18629
Maidul Islam <maidul98@gmail.com>
2019-06-10 19:26:58 -0400

    ml fixed case sense

--- app/Http/Controllers/NewsFeedController.php
+++ app/Http/Controllers/NewsFeedController.php
@@ -45,8 +45,8 @@ class NewsFeedController extends Controller
                 // Prepare the request for recombee server, we need 10 recommended items for a given user.
                 $recommendations = Laracombee::recommendTo(Auth::user(), 10, [ //optional parameters:
                     'filter' => "'user_id' != ".Auth::user()->id."",
-                    'diversity'=> '.35',
-                    'rotationRate'=> '1'
+                    // 'diversity'=> '1',
+                    // 'rotationRate'=> '1'
                   ])->wait();
                 $recc_id = $recommendations['recommId'];
                 $reccs = $recommendations['recomms'];
